2d21f6ba16b62d28b809dd2a8f6dabd41c3ae665
Christina Zakarian <31866115+czakarian@users.noreply.github.com>
2022-02-24 15:05:38 -0800

    added parsing and drawing to MotifMark class

--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -73,7 +73,129 @@ class Sequence:
                 end_found = True       
         self.exon_start = start
         self.exon_length = end - start 
-    
+
+class MotifMark:
+    def __init__(self, fasta, motifs):
+        '''This class parses the fasta and motif files and uses pycairo to generate the image.'''
+
+        self.fasta = fasta
+        self.motifs = motifs
+        self.seq_objs = []
+        self.get_seq_objs()
+        self.motif_objs = []
+        self.get_motif_objs()
+
+        self.surface = cairo.SVGSurface("seq.svg", 1000, 1000)
+        self.context = cairo.Context(self.surface)
+        self.line_start_x = 50
+        self.line_start_y = 100 
+
+    def get_seq_objs(self):
+        # parse fasta and generate list of Sequence objects
+        
+        # turn multiline fasta seqs into one-liners 
+        ol_output = "OL_" + fasta
+        Bioinfo.oneLineFasta(fasta, ol_output)    
+
+        header = ""
+        with open(ol_output, "r") as fr:
+            for line in fr:
+                line = line.strip()
+                if line[0] == ">":
+                    header = line
+                else:
+                    self.seq_objs.append(Sequence(header, line))
+
+    def get_motif_objs(self):
+        # parse motif file and generate list of Motif objects
+        with open(motifs, "r") as fr:
+            for line in fr:
+                line = line.strip()
+                self.motif_objs.append(Motif(line))
+ 
+    def draw_header(self, seq):
+        """This function..."""
+        self.context.set_source_rgb(0, 0, 0)
+        self.context.set_font_size(13)
+        self.context.move_to(self.line_start_x, self.line_start_y - 20)
+        self.context.show_text(seq.header)
+
+    def draw_line(self, seq):
+        """This function..."""
+        # draw sequence line
+        seq_length = len(seq.sequence)
+        line_end_x = seq_length + self.line_start_x 
+        line_end_y = self.line_start_y 
+
+        self.context.set_source_rgb(0, 0, 0)
+        self.context.set_line_width(1)
+        self.context.move_to(self.line_start_x, self.line_start_y) 
+        self.context.line_to(line_end_x, line_end_y)
+        self.context.stroke()
+
+    def draw_exon(self, seq):
+        """This function..."""
+        # draw exon
+        exon_start = seq.exon_start
+        exon_length = seq.exon_length
+        rect_start_x = exon_start  
+        rect_start_y = self.line_start_y - 10
+        rect_length = exon_length 
+        rect_width = 20
+
+        self.context.set_source_rgb(0, 0, 0)
+        self.context.rectangle(rect_start_x, rect_start_y, exon_length, rect_width)
+        self.context.fill()
+
+    def draw_motifs(self, seq):
+        """This function..."""
+
+        colors = np.array([(189,69,71), (71,122,198), (69,150,62), (214,145,33), (151,79,176)])/255
+
+        for c,m in enumerate(self.motif_objs):
+            positions = m.find_motif(seq)
+            m.color = colors[c]
+            print(seq.header)
+            print(m.motif)
+            print(positions)
+            self.context.set_source_rgb(m.color[0], m.color[1], m.color[2])
+            for p in positions:     
+                self.context.rectangle(self.line_start_x + p, self.line_start_y - 10, len(m.motif), 20)
+                self.context.fill()
+        self.line_start_y += 60 
+
+    def draw_legend(self):
+        # make legend
+        leg_pos_x = 50 
+        leg_pos_y = 45
+        self.context.move_to(leg_pos_x, leg_pos_y)   
+        self.context.set_source_rgb(0, 0, 0)
+        self.context.set_font_size(13)
+        self.context.select_font_face("Courier", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+        self.context.show_text("Motifs:")
+        leg_pos_x += 60
+        for m in self.motif_objs:
+            self.context.rectangle(leg_pos_x, leg_pos_y - 11, 15, 15) 
+            self.context.set_source_rgb(m.color[0], m.color[1], m.color[2])
+            self.context.fill()
+            self.context.move_to(leg_pos_x + 20, leg_pos_y)   
+            self.context.show_text(m.motif)
+            leg_pos_x += 100
+
+    def generate_image(self):
+        """This function ..."""  
+        for s in self.seq_objs:
+            self.draw_header(s)
+            self.draw_line(s)
+            self.draw_exon(s)
+            self.draw_motifs(s)
+        self.draw_legend()   
+   
+        out_filename = fasta.split('.')[0]
+        self.surface.write_to_png(out_filename + '.png')
+        self.surface.finish()
+
+
 def get_args():
     """This function returns the parser arguments entered in command line"""
     parser = argparse.ArgumentParser(description="A program to visualize motifs on sequences")
@@ -86,100 +208,7 @@ args = get_args()
 fasta= args.fasta
 motifs = args.motifs
 
-# get the filename without extension to generate output filename
-out_filename = fasta.split('.')[0]
 
-# turn multiline fasta sequences in one-liners
-ol_output = "OL_" + fasta
-Bioinfo.oneLineFasta(fasta, ol_output)
 
-# parse fasta and generate list of Sequence objects
-seq_objs = []
-header = ""
-with open(ol_output, "r") as fr:
-    for line in fr:
-        line = line.strip()
-        if line[0] == ">":
-            header = line
-        else:
-            seq_objs.append(Sequence(header, line))
-
-# parse motif file and generate list of Motif objects
-motif_objs = []
-with open(motifs, "r") as fr:
-    for line in fr:
-        line = line.strip()
-        motif_objs.append(Motif(line))
-
-# draw 
-surface_height = len(seq_objs)*100
-surface = cairo.SVGSurface(out_filename + ".svg", 1100, surface_height)
-context = cairo.Context(surface)
-
-line_start_x = 15
-line_start_y = 100 
-
-for seq in seq_objs:
-    # add header text
-    context.set_source_rgb(0, 0, 0)
-    context.set_font_size(13)
-    context.move_to(line_start_x, line_start_y - 20)
-    context.show_text(seq.header)
-
-    # draw sequence line
-    seq_length = len(seq.sequence)
-    line_end_x = seq_length + line_start_x 
-    line_end_y = line_start_y 
-
-    context.set_source_rgb(0, 0, 0)
-    context.set_line_width(1)
-    context.move_to(line_start_x, line_start_y) 
-    context.line_to(line_end_x, line_end_y)
-    context.stroke()
-
-    # draw exon
-    exon_start = seq.exon_start
-    exon_length = seq.exon_length
-    rect_start_x = exon_start  
-    rect_start_y = line_start_y - 10
-    rect_length = exon_length 
-    rect_width = 20
-
-    context.set_source_rgb(0, 0, 0)
-    context.rectangle(rect_start_x, rect_start_y, exon_length, rect_width)
-    context.fill()
-
-    # draw motifs 
-    colors = np.array([(189,69,71), (71,122,198), (69,150,62), (214,145,33), (151,79,176)])/255
-
-    for c,m in enumerate(motif_objs):
-        positions = m.find_motif(seq)
-        m.color = colors[c]
-        print(seq.header)
-        print(m.motif)
-        print(positions)
-        context.set_source_rgb(m.color[0], m.color[1], m.color[2])
-        for p in positions:     
-            context.rectangle(line_start_x + p, rect_start_y, len(m.motif), rect_width)
-            context.fill()
-    line_start_y += 60 
-
-# make legend
-leg_pos_x = 15 
-leg_pos_y = 45
-context.move_to(leg_pos_x, leg_pos_y)   
-context.set_source_rgb(0, 0, 0)
-context.set_font_size(13)
-context.select_font_face("Courier", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
-context.show_text("Motifs:")
-leg_pos_x += 60
-for m in motif_objs:
-    context.rectangle(leg_pos_x, leg_pos_y - 11, 15, 15) 
-    context.set_source_rgb(m.color[0], m.color[1], m.color[2])
-    context.fill()
-    context.move_to(leg_pos_x + 20, leg_pos_y)   
-    context.show_text(m.motif)
-    leg_pos_x += 100
-
-surface.write_to_png(out_filename + '.png')
-surface.finish()
+mm = MotifMark(fasta, motifs)
+mm.generate_image()
